f2e69b7a94d138636987b15fde2978a71672ba08
Dromarch <41089281+Dromarch@users.noreply.github.com>
2018-07-19 01:44:37 -0300

    EsteemSkipTItle

---
+++ EsteemSkipTItle.js
@@ -0,0 +1,25 @@
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ✦ Esteem System - Skip Title
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/*:
+* @plugindesc Esse plugin faz com que o MV pule a tela de título indo diretamente para o primeiro mapa.
+*
+* @author Dromarch "Skyloftian"
+*
+* @help Para ativar o efeito do plugin basta deixar seu status definido como ON.   
+*
+*/
+
+    Scene_Title.prototype.start = function() {
+        DataManager.setupNewGame();
+        SceneManager.goto(Scene_Map);
+    };
+    
+    Scene_Title.prototype.create = function() {
+    };
+
+    Scene_Title.prototype.update = function() {
+    };
+
+    Scene_Title.prototype.isBusy = function() {
+    };
